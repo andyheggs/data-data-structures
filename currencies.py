62c176c9b675ec84f7e6d58daf6c88d88ca2ea31
andy heggs <andy@heggs.net>
2025-01-28 14:19:34 +0000

    3-Data_Structures-1st Commit

--- currencies.py
+++ currencies.py
@@ -1,11 +1,36 @@
 # pylint: disable=missing-docstring
 
 # TODO: add some currency rates
-RATES = {}
+
+"""
+1.  Create a new constant dictionary called RATES at the top of currencies.py.
+    Keys will be 6-letter strings like "USDEUR", "GBPEUR", "CHFEUR"
+    The values will be their rate stored as a simple Python float number.
+"""
+
+RATES = {
+        USDEUR: 0.85,
+        GBPEUR: 1.13,
+        CHFEUR: 0.86,
+        EURGBP: 0.885
+}
+
+"""
+2.  Implement the convert(amount, currency) function.
+    The first parameter is a tuple of two elements: a float and a currency (e.g. (100, "USD")).
+    The second parameter is a String, the currency you want to convert the amount into.
+
+3.  You should round the results to the nearest whole number.
+
+4.  When called with an unknown rate (e.g. "RMBEUR"), the convert function should return None.
+"""
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
     currency is a string
     """
-    pass  # YOUR CODE HERE
+
+
+
+ print(convert((100, "EUR"), "USD"))
